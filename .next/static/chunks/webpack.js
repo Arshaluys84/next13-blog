cb0a27da183f835c712b40e44e191d5ff802a26c
Arshaluys <arshaluys@Arshaluyss-Air.home>
2023-08-30 23:23:31 +0400

    add files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2f280863b6a52c6d"; }
+/******/ 		__webpack_require__.h = function() { return "4f35123ddb18a039"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
